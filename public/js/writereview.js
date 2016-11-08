c26cf3942fe13fb6bd46225608a2411f25374e63
Nelson Ramirez <nelsonr462@gmail.com>
2016-11-08 08:15:04 +0000

    svetabodapati: Working on hooking up the dish page to the database

--- public/js/writereview.js
+++ public/js/writereview.js
@@ -1,3 +1,4 @@
+//Basic Select2 functionality
 $(document).ready(function() {
 	$(".myselect").select2();
 });
--- routes/main.js
+++ routes/main.js
@@ -5,11 +5,20 @@ const db = firebase.database()
 
 module.exports.index = function(req, res) {
 	res.renderT('index', {
-		template: 'index'
+		template: 'index',
+		dishes: dishes,
+		restaurants: hotrestaurants,
+		recents: recents
 	})
 }
 
 module.exports.dish = function(req, res) {
+	res.renderT('dish', {
+		template: 'dish'
+	})
+}
+
+module.exports.restaurant = function(req, res) {
 	res.renderT('dish', {
 		template: 'dish'
 	})
@@ -83,4 +92,41 @@ var refRes = db.ref("Resturants");
 refRes.orderByChild("name").on("child_added", function (snapshot){
 	restaurant= {name: snapshot.val().name};
 	restaurants.push(restaurant);
+});
+
+//Sveta
+//Add Item Reviews
+var reviews = [];
+var reviews;
+var refRev = db.ref("Reviews");
+refRes.orderByChild("name").on("child_added", function (snapshot){
+	restaurant= {name: snapshot.val().name};
+	restaurants.push(restaurant);
+});
+
+//Index: Hot dishes
+var dishes = [];
+var dish;
+refDishes.orderByChild("averageRating").limitToFirst(10).on("child_added", function (snapshot) {
+	dish = {name: snapshot.val().name, desc: snapshot.val().food_description};
+	dishes.push(dish);
+});
+
+//Index: Hot Restaurants
+var hotrestaurants = [];
+refRes.orderByChild("name").on("child_added", function (snapshot){
+	hotrestaurants.push({name:snapshot.val().name, address: snapshot.val().address});
+});
+
+//Index: Recent? Change to date
+var recents = [];
+refDishes.orderByChild("number_of_ratings").limitToFirst(10).on("child_added", function (snapshot){
+	recents.push({name:snapshot.val().name, desc: snapshot.val().food_description});
+});
+
+//Sveta
+//Dish: Reviews
+var reviews = [];
+refRev.orderByChild("name").on("child_added", function (snapshot){
+	reviews.push({name:snapshot.val().reviewer_name, title: snapshot.val().title, body:snapshot.val().body, rating:snapshot.val().rating});
 });
