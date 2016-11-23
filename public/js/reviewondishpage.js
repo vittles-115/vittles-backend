d0f5532e6793b0e5ebc68a365e7b758da966d029
Nelson Ramirez <nelsonr462@gmail.com>
2016-11-23 09:49:24 +0000

    svetabodapati: started working on the writereview stuff on the dish page

---
+++ public/js/reviewondishpage.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	var searchType = "none";
+	
+	$("#s1, #s2, #s3, #s4, #s5").click(function(event) {
+		$("#reviewheader").css("display", "none");
+		$("#top-divider").css("display", "none");
+		$("#bottom-divider").css("display","block");
+		$("#onpage-reviewpost").css("display","block");
+		$("#reviewbox").css("width","800px");
+	})
+	
+	/*
+	$("#restToggle").click(function(event) {
+
+	})
+	
+	$("#query").keypress(function (e) {
+		if (e.which == 13) {
+			if($("#query").val() == "") {
+				alert("Enter a valid search")
+				return
+			}
+		  	var params = {
+				type: searchType,
+				query: $("#query").val()
+			}
+			window.location.href = "/search?type="+params.type+"&query="+params.query
+		}
+	});*/
+
+
+})
